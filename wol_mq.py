c9cec20af97ced1745d5faa8b0d67eb28f4ba2fc
cgy233 <1781387847@qq.com>
2022-10-19 12:58:32 +0800

    wol wake for lan

---
+++ wol_mq.py
@@ -0,0 +1,47 @@
+# python3.6
+
+import random
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'bemfa.com'
+port = 9501
+topic = "ethanpc002"
+# generate client ID with pub prefix randomly
+client_id = '2f13e215aec14d059745eb027aea6d47'
+
+def wol():
+    pass
+    
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
